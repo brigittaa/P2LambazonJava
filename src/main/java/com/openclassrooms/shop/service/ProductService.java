e02d8ca49de34dc4622c65a848b2161e10a6fc75
brigittaa <46249145+brigittaa@users.noreply.github.com>
2019-02-02 09:07:55 +0100

    Create method getProductById() to return a product from the inventory

--- src/main/java/com/openclassrooms/shop/service/ProductService.java
+++ src/main/java/com/openclassrooms/shop/service/ProductService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -44,7 +45,9 @@ public class ProductService {
 	public Product getProductById(Long productId)
 	{
 		// TODO implement the method
-		return null;
+		Optional<Product> product = getAllProducts().stream().filter(x ->x.getId().equals(productId)).findFirst();
+		
+		return product.get();
 
 	}
 
