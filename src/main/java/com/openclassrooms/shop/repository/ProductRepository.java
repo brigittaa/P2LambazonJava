2ea040e425a98f0da261640f829173a3b0438252
brigittaa <46249145+brigittaa@users.noreply.github.com>
2019-02-08 06:04:34 +0100

    Add comments for changes made in method findAll()

--- src/main/java/com/openclassrooms/shop/repository/ProductRepository.java
+++ src/main/java/com/openclassrooms/shop/repository/ProductRepository.java
@@ -10,52 +10,50 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
-
+ * 
  * @author stanlick
  *
  */
 
 @Repository
 public class ProductRepository {
-    private static List<Product> products;
+	private static List<Product> products;
 
-    public ProductRepository()
-    {
-        products = new ArrayList<>();
-        generateProductData();
-    }
+	public ProductRepository() {
+		products = new ArrayList<>();
+		generateProductData();
+	}
 
-    /**
-     * Generate the default list of products
-     */
-    private void generateProductData()
-    {
-        long id = 0;
-        products.add(new Product(++id, 10, 92.50, "Echo Dot", "(2nd Generation) - Black"));
-        products.add(new Product(++id, 20, 9.99, "Anker 3ft / 0.9m Nylon Braided", "Tangle-Free Micro USB Cable"));
-        products.add(new Product(++id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
-        products.add(new Product(++id, 40, 32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
-        products.add(new Product(++id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
-    }
+	/**
+	 * Generate the default list of products
+	 */
+	private void generateProductData() {
+		long id = 0;
+		products.add(new Product(++id, 10, 92.50, "Echo Dot", "(2nd Generation) - Black"));
+		products.add(new Product(++id, 20, 9.99, "Anker 3ft / 0.9m Nylon Braided", "Tangle-Free Micro USB Cable"));
+		products.add(new Product(++id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
+		products.add(new Product(++id, 40, 32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
+		products.add(new Product(++id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
+	}
 
-    /**
-     * @return All products from the inventory
-     */
-    public List<Product> findAll()
-    {
-        return products.stream().filter(p -> p.getStock() > 0).sorted(Comparator.comparing(Product::getName)).collect(Collectors.toList());    
-        }
+	/**
+	 * @return All products from the inventory
+	 */
+	public List<Product> findAll() { /**change return type from array to list**/
+		return products.stream().filter(p -> p.getStock() > 0).sorted(Comparator.comparing(Product::getName))
+				.collect(Collectors.toList()); /**Collect stream elements into a list instance**/
+	}
 
-    /**
-     * @param productId ID of the getProductById
-     * @param quantityToRemove Quantity of the getProductById
-     */
-    public void updateProductStocks(int productId, int quantityToRemove) {
-        Product product = products.stream().filter(p -> p.getId() == productId).findFirst().get();
-        product.setStock(product.getStock() - quantityToRemove);
+	/**
+	 * @param productId        ID of the getProductById
+	 * @param quantityToRemove Quantity of the getProductById
+	 */
+	public void updateProductStocks(int productId, int quantityToRemove) {
+		Product product = products.stream().filter(p -> p.getId() == productId).findFirst().get();
+		product.setStock(product.getStock() - quantityToRemove);
 
-        if (product.getStock() == 0){
-            products.remove(product);
-        }
-    }
+		if (product.getStock() == 0) {
+			products.remove(product);
+		}
+	}
 }
