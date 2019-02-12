52e0b9912e02f03c5e592bbd0cb3c0e66c6af189
brigittaa <46249145+brigittaa@users.noreply.github.com>
2019-02-12 06:36:52 +0100

    Format indentation and comments

--- src/main/java/com/openclassrooms/shop/domain/Cart.java
+++ src/main/java/com/openclassrooms/shop/domain/Cart.java
@@ -6,14 +6,15 @@ import java.util.Optional;
 
 public class Cart {
 	
-	List <CartLine> actualCartList = new ArrayList<CartLine>(); /**Create an empty list of type Cartline with the name of actualCartList**/
-
+	/**Create an empty list of type Cartline with the name of actualCartList**/	
+	List <CartLine> actualCartList = new ArrayList<CartLine>(); 
+	
     /**
      *
      * @return the actual cartline list
      */
     public List<CartLine> getCartLineList() {
-        return actualCartList;    /** return actualCartList**/
+        return actualCartList;   
     }
 
     /**
@@ -23,18 +24,28 @@ public class Cart {
      */
     public void addItem(Product product, int quantity) {
         // TODO implement the method
-    	CartLine cartline = new CartLine(); /**create an object of class CartLine**/
     	
+    	/**create an object of class CartLine**/
+    	CartLine cartline = new CartLine(); 
+    	
+    	/**
+    	 * find the first element which matches with  @param product from a stream
+    	 * store it in optional object with the name of optionalProduct 
+    	 **/
     	Optional<CartLine> optionalProduct = actualCartList.stream().filter(x -> x.getProduct().getId().equals(product.getId())).findFirst();
-    	/**find the first element which matches with  @param product from a stream and store it in optional object with the name of optionalProduct **/
     	
-    	if(!(optionalProduct.isPresent())) { /**if value not present**/
-    		cartline.setProduct(product);   /**set the value of the Product product to @param product **/
-    		cartline.setQuantity(quantity); /**set the value of the quantity variable to @param quantity**/
-    		actualCartList.add(cartline); /**add the cartline to the actual cart list**/
-    	} else { /**if value is present**/
-    		optionalProduct.get().setQuantity(optionalProduct.get().getQuantity() + quantity); /**get the already added product and increment its quantity with @param quantiy**/
-    		
+    	/**if value not present**/
+    	if(!(optionalProduct.isPresent())) {
+    		/**set the value of the Product product to @param product **/
+    		cartline.setProduct(product);  
+    		/**set the value of the quantity variable to @param quantity**/
+    		cartline.setQuantity(quantity); 
+    		/**add the cartline to the actual cart list**/
+    		actualCartList.add(cartline);   
+    		/**if value is present**/
+    		} else { 
+    	    /**get the already added product and increment its quantity with @param quantiy**/
+    		optionalProduct.get().setQuantity(optionalProduct.get().getQuantity() + quantity); 	
     	}
     }
 
@@ -50,41 +61,58 @@ public class Cart {
     /**
      * @return total value of a cart
      */
-    public double getTotalValue()
-    {
+    public double getTotalValue()  {
          //TODO implement the method
-    	double total =0; /**create variable of type double**/
-    	for (CartLine list: actualCartList) { /**loop through the actual cart*/
-    		total += list.getSubtotal();  /**get the subtotal of each cartline,  add them together and store the value in variable total**/
+    	
+    	/**create variable of type double**/
+    	double total =0; 
+    	
+    	/**loop through the actual cart*/
+    	for (CartLine list: actualCartList) { 
+    		/**get the subtotal of each cartline,  add them together and store the value in variable total**/
+    		total += list.getSubtotal();  
     	}
-        return total; /**return the value of total**/
-
+        return total; 
     }
 
     /**
      * @return Get average value of a cart
      */
-    public double getAverageValue()
-    {
+    public double getAverageValue() {
         // TODO implement the method
-
-    	int quantity=0; /**create variable of type int**/
-    	for (CartLine list: actualCartList) { /**loop through the actual cart **/
-    		quantity = quantity + list.getQuantity(); /**get the quantity of each cartline, add them together and store the value in quantity variable**/
+    	
+    	/**create variable of type int**/
+    	int quantity=0; 
+    	
+    	/**loop through the actual cart **/
+    	for (CartLine list: actualCartList) { 
+    		/**get the quantity of each cartline, add them together and store the value in quantity variable**/
+    		quantity = quantity + list.getQuantity(); 
     	}
-    	return getTotalValue()/quantity; /**divide total value of the cart with the total quantity and return its value**/
+    	/**divide total value of the cart with the total quantity and return its value**/
+    	return getTotalValue()/quantity; 
     }
 
     /**
      * @param productId the getProductById id to search for
      * @return getProductById in the cart if it finds it
      */
-    public Product findProductInCartLines(Long productId)
-    {
+    public Product findProductInCartLines(Long productId) {
         // TODO implement the method
+    	
+    	/**
+    	 * find the first element by productID from a stream 
+    	 * store it in optional object with the name of product 
+    	 */
     	Optional <CartLine> product = actualCartList.stream().filter(x ->x.getProduct().getId().equals(productId)).findFirst();
-    	/**find the first element by productID from a stream and store it in optional object with the name of product **/
-        return product.get().getProduct(); /**return product**/
+    	
+    	/**if product present**/
+    	if(product.isPresent()) {
+    		return product.get().getProduct();
+    	} else {
+    		return null;
+    	}
+         
     }
 
     /**
@@ -92,16 +120,14 @@ public class Cart {
      * @param index index of the cartLine
      * @return CartLine in that index
      */
-    public CartLine getCartLineByIndex(int index)
-    {
+    public CartLine getCartLineByIndex(int index) {
         return getCartLineList().get(index);
     }
 
     /**
      * Clears a the cart of all added products
      */
-    public void clear()
-    {
+    public void clear() {
         List<CartLine> cartLines = getCartLineList();
         cartLines.clear();
     }
